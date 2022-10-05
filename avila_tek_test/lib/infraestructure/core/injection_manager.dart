db93f02bd6e4877cf936cb3531bf70e7b94a361d
Carlos Doffiny S-V <cdoffiny@gcf-corp.com>
2022-10-04 20:52:42 -0400

    Added the repository manager basic configs

--- avila_tek_test/lib/infraestructure/core/injection_manager.dart
+++ avila_tek_test/lib/infraestructure/core/injection_manager.dart
@@ -2,6 +2,7 @@
 import 'package:get_it/get_it.dart';
 
 //Project imports
+import 'package:avila_tek_test/infraestructure/core/repository_manager.dart';
 import 'package:avila_tek_test/infraestructure/core/navigator_manager.dart';
 import 'package:avila_tek_test/infraestructure/core/context_manager.dart';
 
@@ -11,8 +12,10 @@ final getIt = GetIt.instance;
 class InjectionManager {
   static void setupInjections() async {
     getIt.registerSingleton<ContextManager>(ContextManager());
-    // getIt.registerSingleton<RepositoryManager>(RepositoryManager());
+    getIt.registerSingleton<RepositoryManager>(RepositoryManager());
 
     NavigatorServiceContract.inject();
+
+    //USE CASES INJECTION
   }
 }
